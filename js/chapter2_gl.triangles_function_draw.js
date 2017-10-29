c1da8edd7f7abf1801e962233e9a1abc64fc2fbb
niksharapov <sarapovk4@gmail.com>
2017-10-30 00:25:45 +0500

    К html-файлу "Создание gl.triangles" второй главы добавлен скрипт - функция рисования

---
+++ js/chapter2_gl.triangles_function_draw.js
@@ -0,0 +1,14 @@
+// отрисовка
+function draw() {
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
+        vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    // отрисовка треугольников
+    gl.drawElements(gl.TRIANGLES, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT,0);
+}
