25817abdf7fbf62c3910c04cd59f32492accbdf2
niksharapov <sarapovk4@gmail.com>
2017-10-30 00:41:28 +0500

    Ко всем html-файлам второй главы добавлен скрипт-функция окно процесса

---
+++ js/chapter2_function_window.onload.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+
+    var canvas = document.getElementById("canvas3D");
+    try {
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    }
+    catch(e) {}
+
+    if (!gl) {
+        alert("Ваш браузер не поддерживает WebGL");
+    }
+    if(gl){
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+        initShaders();
+
+        initBuffers();
+
+        draw();
+    }
+}
