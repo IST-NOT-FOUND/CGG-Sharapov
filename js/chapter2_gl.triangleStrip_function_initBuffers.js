cc0dcf6c30001e6712ae67c338f57b709f76ec37
niksharapov <sarapovk4@gmail.com>
2017-10-30 00:31:30 +0500

    К html-файлу "Создание gl.triangleStrip" второй главы добавлен скрипт - функция установки буффера вершин

---
+++ js/chapter2_gl.triangleStrip_function_initBuffers.js
@@ -0,0 +1,18 @@
+// установка вершинного буфера
+function initBuffers() {
+
+    vertices =[ -0.5,  -0.5,  0.0, //m0
+        -0.5,  0.5,  0.0, //m1
+        0.0,  0.0,  0.0, //m2
+        0.5,  0.5,  0.0, //m3
+        0.5,  -0.5,  0.0, //m3
+    ];
+    // установка буфера вершин
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // размерность
+    vertexBuffer.itemSize = 3;
+    // указываем кол-во вершин - 5
+    vertexBuffer.numberOfItems=5;
+}
